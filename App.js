41b64d3fda81188ec198e7f774756a8a879e6bf1
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2017-10-12 02:02:32 +0700

    Initial boilerplate from create-react-native-app

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <View style={styles.container}>
+        <Text>Hello world Pupuru Moshi-moshi! Molapiyo ... There was once a lonely wolf that is hunting alone in the deep dark forest.</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
