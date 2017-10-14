206a995a29cbfe79219533eea875a086c0e6dbc4
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2017-10-14 22:40:15 +0700

    Add a TextInput

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
+import { AppRegistry, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 class GreetingPupuru extends Component {
   render() {
@@ -31,12 +31,19 @@ class BlinkPupuru extends Component {
 }
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  };
+
   render() {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
     return (
       <View style={styles.container}>
+        <TextInput style={{height: 40}} placeholder='Type here to translate!' onChangeText={(text) => this.setState({text})}></TextInput>
+        <Text style={{padding: 10, fontSize: 20}}>{this.state.text + ' pupuru'}</Text>
         <Text>Hello world Pupuru Moshi-moshi! Molapiyo ... There was once a lonely wolf that is hunting alone in the deep dark forest.</Text>
         <Image source={pic} style={{width: 193, height: 110}} />
         <GreetingPupuru name='Jon Snow' class='Night Watch Ranger' />
@@ -45,9 +52,11 @@ export default class App extends Component {
         <Text style={styles.bigblue}>Just Big Blue</Text>
         <Text style={[styles.bigblue, styles.red]}>Big Blue, then Red</Text>
         <Text style={[styles.red, styles.bigblue]}>Red, then Big Blue</Text>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+          <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+          <View style={{flex: 1, backgroundColor: 'steelblue'}} />
+        </View>
       </View>
     );
   }
@@ -57,6 +66,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 10,
   },
   bigblue: {
     color: 'blue',
