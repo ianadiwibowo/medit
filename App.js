909876d6d10567fca29b24f985157d00cace4ee1
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2017-10-18 23:28:00 +0700

    Add SectionList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, FlatList, SectionList, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends Component {
   render() {
@@ -25,6 +25,15 @@ export default class App extends Component {
           ]}
           renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
         />
+        <SectionList
+          sections={[
+              {title: 'S', data: ['Senja']},
+              {title: 'E', data: ['Eddy']},
+              {title: 'A', data: ['Aya', 'Aldi', 'Alfi']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+        />
       </View>
     );
   }
@@ -40,5 +49,14 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 18,
     height: 44
-  }
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
 });
