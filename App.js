7bdeebcc3f245e907a12cbfd4c6ffc8967ac04a1
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2017-10-25 23:10:47 +0700

    Try fetch

--- App.js
+++ App.js
@@ -1,73 +1,45 @@
 import React, { Component } from 'react';
-import { AppRegistry, FlatList, SectionList, StyleSheet, Text, View } from 'react-native';
-
-async function getMoviesFromApi() {
-  try {
-    let response = await fetch('https://facebook.github.io/react-native/movies.json');
-    let responseJson = await response.json();
-    return responseJson.movies;
-  }
-  catch (error) {
-    console.log(error);
-  }
-}
+import { ActivityIndicator, AppRegistry, ListView, Text, View } from 'react-native';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true };
+  }
+
+  componentDidMount() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          isLoading: false,
+          dataSource: ds.cloneWithRows(responseJson.movies)
+        }, function() {
+          // do something with new state
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
-      <View style={styles.container}>
-        <FlatList
-          data={[
-            {key: 1, name: 'Senja'},
-            {key: 2, name: 'Reston'},
-            {key: 3, name: 'Eddy'},
-            {key: 4, name: 'Aldi'},
-            {key: 5, name: 'Alfi'},
-            {key: 6, name: 'Budi'},
-            {key: 7, name: 'Ridwan'},
-            {key: 8, name: 'Rizqi'},
-            {key: 9, name: 'Ifa'},
-            {key: 10, name: 'Aya'},
-            {key: 11, name: 'Ian'},
-            {key: 12, name: 'Kelvin'},
-            {key: 13, name: 'Yola'},
-            {key: 14, name: 'Arka'},
-            {key: 15, name: 'Edwin'}
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
-        />
-        <SectionList
-          sections={[
-              {title: 'S', data: ['Senja']},
-              {title: 'E', data: ['Eddy']},
-              {title: 'A', data: ['Aya', 'Aldi', 'Alfi']},
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+      <View style={{flex: 1, paddingTop: 20}}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData.title}, {rowData.releaseYear}</Text>}
         />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 20
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44
-  },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
-  },
-});
